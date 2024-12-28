7791fde878b14138a660cf69a34abbe24a6b817e
Glenvin <glenrosario84@gmail.com>
2024-12-29 01:37:19 +0530

    implemented contacts page including bg image

--- src/app/contacts/TacDesignLab.tsx
+++ src/app/contacts/TacDesignLab.tsx
@@ -7,17 +7,31 @@ const TacDesignLab = () => {
     <>
       <div className={styles.tacDesignContainer}>
         <section className={styles.smallContainer}>
-          <img
-            src="./images/arrowImage.png"
-            alt="Arrow Image"
-            className={styles.arrowImage}
-          />
-          <p className={styles.tacText}>TAC Design LAB</p>
-          <p className={styles.tacDescription}>
-            Stay connected with us and explore our journey towards
-            sustainability. Follow us on our social hubs for updates, projects,
-            and inspiration!
-          </p>
+          <span>
+            <img
+              src="./images/contactImage.jpeg"
+              alt="Contact Image"
+              className={styles.imageStyle}
+            />
+          </span>
+          <section className={styles.sectionContainer}>
+            {/* Title line with arrow image and text */}
+            <div className={styles.titleContainer}>
+              <img
+                src="./images/arrowImage.png"
+                alt="Arrow Image"
+                className={styles.arrowImage}
+              />
+              <p className={styles.tacText}>TAC Design LAB</p>
+            </div>
+
+            {/* Description text */}
+            <p className={styles.tacDescription}>
+              Stay connected with us and explore our journey towards
+              sustainability. Follow us on our social hubs for updates,
+              projects, and inspiration!
+            </p>
+          </section>
         </section>
       </div>
     </>
--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,32 +1,45 @@
 import React from 'react';
 
+import { getOptimizedBackgroundImage } from '~/utils/background-image-optimizer';
+
+import contactPageBackground from '~/assets/images/contactusBG.png';
+
 import styles from './Contact.module.css';
 import EmailSection from './EmailSection';
+import ScoialMediaSection from './ScoialMediaSection';
+import TacDesignLab from './TacDesignLab';
 import TopSection from './TopSection';
 
 const Contacts = () => {
+  const optimizedCareerPageBackground = getOptimizedBackgroundImage({
+    src: contactPageBackground.src,
+    width: contactPageBackground.width,
+    height: contactPageBackground.height,
+  });
   return (
-    <>
-      <section>
-        <div>
-          <div className={styles.headerStyles}>
-            <TopSection />
+    <section
+      className="bg-[#EDD7C3] bg-contain py-16 font-primary bg-blend-soft-light"
+      style={{
+        backgroundImage: optimizedCareerPageBackground,
+      }}
+    >
+      <div style={{ padding: '20px 30px 20px 70px', marginTop: '50px' }}>
+        <div className={styles.headerStyles}>
+          <TopSection />
+        </div>
+        <div className={styles.bodyContainer}>
+          <div className={styles.leftConatiner}>
+            <EmailSection />
           </div>
-          <div className={styles.bodyContainer}>
-            {/* div B having div C and D*/}
-            <div className={styles.leftConatiner}>
-              {' '}
-              {/* div C*/}
-              Div C
-            </div>
-            <div className={styles.rightContainer}>
-              {/* div D  */}
-              Div D
+          <div className={styles.rightContainer}>
+            <TacDesignLab />
+            <div style={{ marginLeft: '50px' }}>
+              <ScoialMediaSection />
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
