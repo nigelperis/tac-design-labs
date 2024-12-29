54ad01300cc00e0c9744566b2286ebd0e6c3f49c
Nigel Peris <nperis27@gmail.com>
2024-12-29 18:27:06 +0530

    feat: update achievement links and enhance button styles

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -52,37 +52,37 @@ function Achievement() {
         award celebrates his outstanding contributions to eco-friendly
         construction and sustainable design practices."
           image={achievementPhoto1}
-          href="#"
+          href="https://hudco.org.in/writereaddata/HUDCO-DA23-announcement.pdf"
         />
         <AchievementCard
           title="The Better India: Recognizing Sustainable Innovation"
           description="The Better India featured Ar. Ashams Ravi of TAC Design LAB for his groundbreaking sustainable home in Kerala, constructed using 90% recycled materials and bamboo. This recognition highlights his innovative approach to eco-friendly design, achieving significant environmental..."
           image={achievementPhoto2}
-          href="#"
+          href="https://thebetterindia.com/224686/kerala-architect-sustainable-home-recycled-material-eco-friendly-water-india-ang136/"
         />
         <AchievementCard
           title="Rethinking The Future: Top 10 Examples of Upcycling in Architecture"
           description="TAC Design LAB's work was featured in Rethinking The Future as one of the top 10 examples of upcycling in architecture. Their innovative approach to reusing materials from demolished buildings showcases their commitment to reducing waste and promoting sustainable architecture."
           image={achievementPhoto3}
-          href="#"
+          href=" https://www.re-thinkingthefuture.com/rtf-fresh-perspectives/a1989-10-examples-of-upcycling-in-architecture/"
         />
         <AchievementCard
           title="The New Indian Express: The Green Building Specialist"
           description="Ar. Ashams Ravi, honored with the HUDCO Design Award 2021-2022, was featured by The New Indian Express for his pioneering work in sustainable architecture. His project 'Canaan' in Thiruvananthapuram, constructed with 90% reused materials, highlights his dedication to eco-friendly design and minimal environmental impact."
           image={achievementPhoto4}
-          href="#"
+          href="https://www.newindianexpress.com/cities/kochi/2022/Aug/26/the-green-building-specialist-2491360.html"
         />
         <AchievementCard
           title="Tamil Thisai: Bamboo & Recycled Waste - A Green Home in Kerala"
           description="Tamil Thisai highlighted TAC Design LAB's eco-friendly home in Kerala, constructed using 90% recycled materials and bamboo. Completed in just four months, the project exemplifies the firm's dedication to rapid, sustainable construction that harmonizes with nature."
           image={achievementPhoto5}
-          href="#"
+          href="https://tamilthisai.com/india/bamboo-90-recycled-waste-kerala-architect-builds-green-home-in-4-months/"
         />
         <AchievementCard
           title="TEDx: Sustainable Architecture - A Talk by Ar. Ashams Ravi"
           description="In a TEDx talk, Ar. Ashams Ravi delves into the principles of sustainable architecture, emphasizing eco-conscious design and construction practices. His insights and experiences offer valuable lessons for future architects and designers aiming to minimize environmental impact."
           image={achievementPhoto6}
-          href="#"
+          href=" https://youtu.be/td0RKMoBkuM?si=y2zLy5rfCPaieDjk"
         />
       </section>
 
@@ -98,13 +98,12 @@ function Achievement() {
           </p>
         </div>
 
-        <Link
-          href="#"
+        <button
           style={{ backgroundImage: optimizedButtonBackground }}
-          className="mx-auto mt-4 block w-fit bg-cover bg-center px-[40px] py-[20px] font-primary text-lg font-bold text-[#331803]"
+          className="mx-auto mt-4 block rounded bg-cover bg-center p-8 font-primary text-lg font-bold text-[#331803] duration-200 hover:scale-105"
         >
           Explore Our Services
-        </Link>
+        </button>
       </section>
     </div>
   );
@@ -123,7 +122,7 @@ function AchievementCard(props: AchievementCardProps) {
   const { title, description, image, href } = props;
 
   return (
-    <div className="gap-2 overflow-auto rounded-[20px] bg-[#FFFFFF80] p-5 shadow-lg md:grid md:grid-cols-[2fr_1fr] md:rounded-[48px]">
+    <div className="mb-10 gap-2 overflow-auto rounded-[20px] bg-[#FFFFFF80] p-5 shadow-lg md:grid md:grid-cols-[2fr_1fr] md:rounded-[48px]">
       <h3 className="font-primary text-2xl font-bold text-[#552B0A] md:text-4xl 2xl:self-center">
         {title}
       </h3>
@@ -137,7 +136,7 @@ function AchievementCard(props: AchievementCardProps) {
       </p>
       <Link
         href={href}
-        className="mx-auto mt-5 flex h-fit w-fit items-center space-x-4 rounded-full bg-secondary-500 px-5 py-4 text-center text-sm font-bold text-white md:col-start-1 md:col-end-2"
+        className="mx-auto mt-5 flex h-fit w-fit items-center space-x-4 rounded-full bg-secondary-500 px-5 py-4 text-center text-sm font-bold text-white duration-300 hover:scale-105 md:col-start-1 md:col-end-2"
       >
         Read the full announcement <ChevronRight className="text-[7px]" />
       </Link>
