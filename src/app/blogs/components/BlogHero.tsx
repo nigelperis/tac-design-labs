46255a7d8b29f6aeb7383a36d4b71dd61ad29814
Mir Mubashshir Zahoor <mir.mubashshir@codecraft.co.in>
2025-01-01 17:22:31 +0530

    feat: Add more blog section

--- src/app/blogs/components/BlogHero.tsx
+++ src/app/blogs/components/BlogHero.tsx
@@ -22,7 +22,9 @@ const BlogHero = (props: BlogHeroProps) => {
         boxShadow: '4px 4px 8px 6px #00000040 inset',
       }}
     >
-      <h1 className="text-2xl font-bold md:text-4xl">{props.title}</h1>
+      <h1 className="text-balance px-5 text-center text-2xl font-bold leading-10 md:text-4xl">
+        {props.title}
+      </h1>
       <h3 className="absolute bottom-5 right-5 text-sm leading-[5%] md:text-2xl">
         by Admin | 28 Nov 2024
       </h3>
