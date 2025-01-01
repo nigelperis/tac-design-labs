8d9e4f49089c9e543aedcec64807c8ddacb41403
Mir Mubashshir Zahoor <mir.mubashshir@codecraft.co.in>
2025-01-01 10:36:24 +0530

    feat: add blog 2, 3, 4 MD file

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import React from 'react';
-import { MDXComponents } from 'mdx/types';
+import { use } from 'react';
+import { MDXComponents, MDXProps } from 'mdx/types';
 import { StaticImageData } from 'next/image';
 
+// MARK: Image imports
 import blogPost1 from '~/assets/images/blog-images/blog-post-1.jpg';
+import blogPost2 from '~/assets/images/blog-images/blog-post-2.jpg';
+import blogPost3 from '~/assets/images/blog-images/blog-post-3.png';
+import blogPost4 from '~/assets/images/blog-images/blog-post-4.jpg';
 
-import Blog01 from '../components/blog-01.mdx';
 import BlogHero from '../components/BlogHero';
+// MARK: Markdown imports
+import Blog01 from '../markdown/blog-01.mdx';
+import Blog02 from '../markdown/blog-02.mdx';
+import Blog03 from '../markdown/blog-03.mdx';
+import Blog04 from '../markdown/blog-04.mdx';
 
 const overrideComponents: MDXComponents = {
+  h2: ({ children }) => (
+    <h2
+      className="text-3xl md:text-[36px]"
+      style={{
+        color: '#C6742B',
+        fontWeight: '700',
+        paddingBottom: '23px',
+        lineHeight: '45px',
+      }}
+    >
+      {children}
+    </h2>
+  ),
   h3: ({ children }) => (
     <h3
       className="text-2xl md:text-[28px]"
@@ -47,18 +68,63 @@ const overrideComponents: MDXComponents = {
     </a>
   ),
   img: ({ src, alt }) => <img draggable={false} src={src} alt={alt} />,
+  li: ({ children }) => (
+    <li
+      className="list-inside list-decimal text-base leading-7 md:text-2xl md:leading-10"
+      style={{
+        letterSpacing: '5%',
+        paddingBottom: '23px',
+      }}
+    >
+      {children}
+    </li>
+  ),
 };
 
-export default function Page() {
+type MDXData = (props: MDXProps) => JSX.Element;
+
+interface BlogDetail {
+  title: string;
+  coverImgURL: StaticImageData;
+  markdown: MDXData;
+}
+
+const blogs: Record<string, BlogDetail> = {
+  '01': {
+    title: 'Sustainable building materials in India',
+    coverImgURL: blogPost1,
+    markdown: Blog01,
+  },
+  '02': {
+    title: 'The Benefits of Bamboo in Sustainable Architecture',
+    coverImgURL: blogPost2,
+    markdown: Blog02,
+  },
+  '03': {
+    title:
+      'How to reduce carbon footprint in building design, A Step-by-Step Guide',
+    coverImgURL: blogPost3,
+    markdown: Blog03,
+  },
+  '04': {
+    title: 'How to design energy-efficient homes',
+    coverImgURL: blogPost4,
+    markdown: Blog04,
+  },
+};
+
+export default function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = use(params);
   return (
     <>
       <div className="bg-[#EDD7C3] font-primary">
-        <BlogHero
-          title="Sustainable building materials in India"
-          image={blogPost1}
-        />
+        <BlogHero title={blogs[slug].title} image={blogs[slug].coverImgURL} />
         <div className="p-5 md:p-28">
-          <Blog01 components={overrideComponents} />
+          {blogs[slug].markdown({ components: overrideComponents })}
         </div>
       </div>
     </>
