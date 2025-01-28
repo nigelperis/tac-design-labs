12f9ea98360006ea4908ba7ac239fab5d13c105e
reevan leonal dsouza <ree1dsouza@gmail.com>
2025-01-29 01:01:10 +0530

    feat: develop strapi utility

---
+++ src/models/blogs/blog.ts
@@ -0,0 +1,72 @@
+import type { BlocksContent } from '@strapi/blocks-react-renderer';
+
+interface CoverImageFormat {
+  ext?: string;
+  url?: string;
+  hash?: string;
+  mime?: string;
+  name?: string;
+  path?: string | null;
+  size?: number;
+  width?: number;
+  height?: number;
+  sizeInBytes?: number;
+}
+
+interface CoverImageFormats {
+  large?: CoverImageFormat;
+  small?: CoverImageFormat;
+  medium?: CoverImageFormat;
+  thumbnail?: CoverImageFormat;
+}
+
+interface CoverImage {
+  id: number;
+  documentId: string;
+  name?: string;
+  alternativeText?: string | null;
+  caption?: string | null;
+  width?: number;
+  height?: number;
+  formats?: CoverImageFormats;
+  hash?: string;
+  ext?: string;
+  mime?: string;
+  size?: number;
+  url?: string;
+  previewUrl?: string | null;
+  provider?: string;
+  provider_metadata?: unknown;
+  createdAt?: string;
+  updatedAt?: string;
+  publishedAt?: string;
+}
+
+interface Author {
+  id: number;
+  documentId: string;
+  name: string;
+  createdAt?: string;
+  updatedAt?: string;
+  publishedAt?: string;
+}
+
+interface Blog {
+  id: number;
+  documentId: string;
+  slug: string;
+  title: string;
+  publishedOn?: string;
+  shortDescription: string;
+  mainContent: BlocksContent;
+  coverImage: CoverImage;
+  author?: Author;
+  createdAt?: string;
+  updatedAt?: string;
+  publishedAt?: string;
+}
+
+export interface BlogResponse {
+  data: Blog;
+  meta: unknown;
+}
---
+++ src/utils/strapi-fetch.ts
@@ -0,0 +1,40 @@
+interface StrapiFetchProps {
+  endpoint: `/${string}`;
+  queryParams?: URLSearchParams;
+}
+
+/**
+ * Fetches data from the Strapi API
+ * @param endpoint - The endpoint to fetch from (withouth inclusion of api prefix and should always begin with forward slash '/' )
+ * @param query - The query parameters to add to the url
+ * @returns
+ * @example 
+     const searchParam = new URLSearchParams();
+      searchParam.append('populate[0]', 'signature');
+      searchParam.append('populate[1]', 'coverImage');
+      strapiFetch(\{
+        endpoint: '/landing-page-priest-message',
+        queryParams:searchParam,
+        );
+ */
+export async function strapiFetch<T>({
+  endpoint,
+  queryParams,
+}: StrapiFetchProps): Promise<T | undefined> {
+  if (!process.env.NEXT_PUBLIC_STRAPI_URL) {
+    throw new Error('Base url for strapi not found ');
+  }
+
+  const url = new URL(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api${endpoint}`);
+
+  if (queryParams) {
+    for (const [key, value] of queryParams) {
+      url.searchParams.append(key, value);
+    }
+  }
+
+  const res = await fetch(url.toString());
+  const data = (await res.json()) as unknown as T;
+
+  return data as unknown as T;
+}
